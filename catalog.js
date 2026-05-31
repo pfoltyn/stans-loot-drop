ae23c4a8835374c56b891acadde956dd47a77670
Piotr Foltyn <pfoltyn@apple.com>
2026-05-31 13:33:10 +0100

    Switch to LIVE Stripe Payment Link

--- catalog.js
+++ catalog.js
@@ -15,7 +15,7 @@ export const CURRENCY = "£";
 export const KEYCHAIN = {
   name: "Custom 2-Sided Keychain",
   price: 3,
-  paymentLink: "https://buy.stripe.com/test_28E3cv9zk3RJg6IfSmgMw00",
+  paymentLink: "https://buy.stripe.com/28E3cv9zk3RJg6IfSmgMw00",
 };
 
 export const SECTIONS = [
